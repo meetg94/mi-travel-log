603244747c218c59c4c9f683af3c98bb34465918
bakedegg <meetguleria94@gmail.com>
2022-01-07 05:28:58 -0800

    v2

--- src/Assets/traveldata.js
+++ src/Assets/traveldata.js
@@ -1,5 +1,6 @@
 export default [
-    {
+    {   
+        id:1,
         title: "Mount Fuji",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -7,6 +8,17 @@ export default [
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         coverImage: "pic1.jpeg",
+        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+    },
+    {   
+        id:2,
+        title: "Tokyo City",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/evsvY8cDYoPNufnMA",
+        startDate: "22 Feb, 2021",
+        endDate: "24 Feb, 2021",
+        description: "Tokyo (東京, Tōkyō) is Japan's capital and the world's most populous metropolis. It is also one of Japan's 47 prefectures, consisting of 23 central city wards and multiple cities, towns and villages west of the city center.",
+        coverImage: "pic2.jpeg",
         imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
     }
 ]
